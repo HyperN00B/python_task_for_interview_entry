6d2b8a44682a0964d16b0ba7c421a64cb18b808f
Tari Bence <bence.tari@gmail.com>
2022-07-01 21:18:20 +0200

    Restructure code, handle empty or invalid inputs and add File compatibility to Task 1

--- Count_Vowels.py
+++ Count_Vowels.py
@@ -1,15 +1,63 @@
-Vowels=["a","e","i","o","u"]
-# Vowels to search for in the input word/file
-VowelsInInput=0
-# Vowel counter set to zero as a failsafe
-InputWord=input("Please enter the word you want to count the vowels in: ")
-# Ask for the input word
-LettersInInput=list(InputWord)
-# Load the input word into a list of individual characters
+import os
+import platform
+import time
 
-def count_vowels(InputWord):
+# Vowels to search for in the input word/file
+Vowels=["a","e","i","o","u"]
+
+# Vowel counter set to zero as a failsafe
+VowelsInInput=0
+
+# Message for empty input
+Empty="I see what you did there! But empty input is not allowed!"
+
+def ClearScreen():
+    # Check the HostOS type to decide which command to use for clearing the Command Prompt/Terminal
+    if platform.system()=="Windows":
+        os.system("cls")
+    else:
+        os.system("clear")
+    pass
+pass
+
+def ask_for_word_or_sentence():
+    ClearScreen()
+    InputParameter=input("Please enter the word you want to count the vowels in!\nYou can also type 'back' to go back to the main screen\nWord/Sentence: ")
+    if not InputParameter:
+        ClearScreen()
+        print(Empty)
+        time.sleep(3)
+        ask_for_word_or_sentence()
+    elif InputParameter.lower()=="back":
+        FeatureSelection()
+    else:
+        print("There are "+count_vowels(InputParameter.lower())+" vowels found in the word or sentence:\n"+InputParameter)
+pass
+
+def ask_for_file():
+    ClearScreen()
+    InputParameter=input("Please enter the file you want to count the vowels in!\nYou can also type 'back' to go back to the main screen\nFile: ")
+    if not InputParameter:
+        ClearScreen()
+        print(Empty)
+        time.sleep(3)
+        ask_for_file()
+    elif InputParameter.lower()=="back":
+        FeatureSelection()
+    elif not os.path.isfile(InputParameter):
+        ClearScreen()
+        print("The input is either not a file, or it doesn't exist!")
+        time.sleep(3)
+        ask_for_file()
+    else:
+        OpenFile=open(InputParameter,"r")
+        ReadFile=OpenFile.read()
+        print("There are "+count_vowels(ReadFile.lower())+" vowels found in the input file:\n"+InputParameter)
+
+def count_vowels(InputParameter):
 # Function definition with word
     global VowelsInInput
+    LettersInInput=list(InputParameter)
     # Make "number of vowels found" a global var so the return statement can actually see it's defined outside of the method.
     for i in LettersInInput:
     # Do it for every letter in the word
@@ -26,9 +74,31 @@ def count_vowels(InputWord):
     # Return the number of vowels found to the main method
 pass
 
-VowelsFound=count_vowels(InputWord)
-# Call the method to do the actual counting
-print("")
-# A newline to make the output a little more spaced
-print("The number of vowels in the given word is: "+VowelsFound)
-# The result of the method
+# Main screen for selecting work method
+def FeatureSelection():
+    ClearScreen()
+    global LettersInInput
+
+    Choice=input("Do you want to use a word/sentence or a file?\n1: Word/Sentence\n2: File\nYour selection: ")
+
+    # The user chose word/sentence
+    if Choice=="1":
+        ask_for_word_or_sentence()
+
+    #The user chose file
+    elif Choice=="2":
+        ask_for_file()
+
+    # Invalid selection, return to Main screen
+    else:
+        ClearScreen()
+        print("Invalid option!")
+        print("The available options are:")
+        print("1: Word/Sentence")
+        print("2: File")
+        time.sleep(3)
+        FeatureSelection()
+    pass
+pass
+
+FeatureSelection()
